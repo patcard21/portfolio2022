6c9112806bab8ded08e6595c29f818d5d0c62f4a
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-30 11:20:32 -0400

    Renamed JS files and reinstalled npm

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import App from "./App.vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
