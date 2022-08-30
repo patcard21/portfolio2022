6a772794be592a6425c21981e2d09efd429cebff
patcard21 <71289158+patcard21@users.noreply.github.com>
2022-08-30 11:52:03 -0400

    Moved my project to the new Vue-created folder

--- portfolio2022/src/main.js
+++ portfolio2022/src/main.js
@@ -6,6 +6,12 @@ import router from "./router";
 
 import "./assets/main.css";
 
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 const app = createApp(App);
 
 app.use(createPinia());
